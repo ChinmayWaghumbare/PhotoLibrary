ee96268717a2f720ff6dfe0b64dc9c3b7f397628
Chinmay Waghumbare <cwaghumbar@gmail.com>
2020-01-10 17:32:25 +0530

    First Commit from office pc

--- PhotoGalleryServise/Controllers/ImagesController.cs
+++ PhotoGalleryServise/Controllers/ImagesController.cs
@@ -9,9 +9,20 @@ namespace PhotoGalleryServise.Controllers
 {
 	public class ImagesController : ApiController
 	{
+		[HttpGet]
 		public string TestImage()
 		{
 			return "In Test";
 		}
+
+		#region Required Methods
+
+		//Get all Images
+		//Get Sorted Images (sort on categories)
+		//Upload Image 
+		//Delete Image
+		//Edit Image (Edit Info of Image (example- change category or any other related info))
+		#endregion
+		//
 	}
 }
