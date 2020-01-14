b60efd357e68b6eb2e67f5b4477f3d0b6b3d968a
Chinmay Waghumbare <cwaghumbar@gmail.com>
2020-01-14 15:16:53 +0530

    Upload File initial

--- PhotoGalleryServise/Controllers/ImagesController.cs
+++ PhotoGalleryServise/Controllers/ImagesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -15,14 +16,66 @@ namespace PhotoGalleryServise.Controllers
 			return "In Test";
 		}
 
-		#region Required Methods
 
-		//Get all Images
-		//Get Sorted Images (sort on categories)
-		//Upload Image 
-		//Delete Image
-		//Edit Image (Edit Info of Image (example- change category or any other related info))
-		#endregion
-		//
-	}
+        [HttpPost()]
+        public string UploadFiles()
+        {
+            int iUploadedCnt = 0;
+
+            // DEFINE THE PATH WHERE WE WANT TO SAVE THE FILES.
+            string sPath = "";
+            sPath = System.Web.Hosting.HostingEnvironment.MapPath("~/App_Data/Images/");
+
+            System.Web.HttpFileCollection hfc = System.Web.HttpContext.Current.Request.Files;
+
+            // CHECK THE FILE COUNT.
+            for (int iCnt = 0; iCnt <= hfc.Count - 1; iCnt++)
+            {
+                System.Web.HttpPostedFile hpf = hfc[iCnt];
+
+                if (hpf.ContentLength > 0)
+                {
+                    // CHECK IF THE SELECTED FILE(S) ALREADY EXISTS IN FOLDER. (AVOID DUPLICATE)
+                    if (!File.Exists(sPath + Path.GetFileName(hpf.FileName)))
+                    {
+                        // SAVE THE FILES IN THE FOLDER.
+                        hpf.SaveAs(sPath + Path.GetFileName(hpf.FileName));
+                        iUploadedCnt = iUploadedCnt + 1;
+                    }
+                }
+            }
+
+            // RETURN A MESSAGE (OPTIONAL).
+            if (iUploadedCnt > 0)
+            {
+                return iUploadedCnt + " Files Uploaded Successfully";
+            }
+            else
+            {
+                return "Upload Failed";
+            }
+        }
+    
+        [HttpGet]
+        public void GetImages()
+        {
+            //Get Image folder path
+            string sPath = "";
+            sPath = System.Web.Hosting.HostingEnvironment.MapPath("~/App_Data/Images/");
+
+            
+            //Get all Images in that folder
+
+            //Return images
+        }
+    #region Required Methods
+
+    //Get all Images
+    //Get Sorted Images (sort on categories)
+    //Upload Image 
+    //Delete Image
+    //Edit Image (Edit Info of Image (example- change category or any other related info))
+    #endregion
+    //
+}
 }
